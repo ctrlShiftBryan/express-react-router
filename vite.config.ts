8ed5880c205b7006a851fe624bf15c0dbf2f7c6a
ctrlShiftBryan <bryanarendt@gmail.com>
2025-02-07 07:31:03 -0500

    Enable source map generation for build process

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig(({ isSsrBuild }) => ({
   build: {
+    sourcemap: true,
     rollupOptions: isSsrBuild
       ? {
         input: "./server/app.ts",
